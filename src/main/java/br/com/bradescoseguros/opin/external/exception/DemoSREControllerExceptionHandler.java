5b73b41827b62a6a4f23363592d3df4ea0656d31
Edson Roberto Faccio Junior <edsonf@ciandt.com>
2023-01-17 15:19:09 -0300

    Criação da exception de Retries Exceeded

--- src/main/java/br/com/bradescoseguros/opin/external/exception/DemoSREControllerExceptionHandler.java
+++ src/main/java/br/com/bradescoseguros/opin/external/exception/DemoSREControllerExceptionHandler.java
@@ -1,14 +1,18 @@
 package br.com.bradescoseguros.opin.external.exception;
 
+import br.com.bradescoseguros.opin.businessrule.exception.demosre.DemoSREMaxRetriesExceededException;
 import br.com.bradescoseguros.opin.businessrule.exception.demosre.DemoSRENoContentException;
 import br.com.bradescoseguros.opin.businessrule.exception.entities.ErrorCode;
+import br.com.bradescoseguros.opin.businessrule.messages.MessageSourceService;
 import br.com.bradescoseguros.opin.external.exception.entities.MetaData;
 import br.com.bradescoseguros.opin.external.exception.entities.MetaDataEnvelope;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -22,6 +26,10 @@ import static org.springframework.util.CollectionUtils.isEmpty;
 @ControllerAdvice
 @RequiredArgsConstructor
 public class DemoSREControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSourceService messageSourceService;
+
     @ExceptionHandler(DemoSRENoContentException.class)
     public ResponseEntity<Object> handleDemoSRENoContentException(final DemoSRENoContentException exception,
                                                          final WebRequest request) {
@@ -34,9 +42,34 @@ public class DemoSREControllerExceptionHandler extends ResponseEntityExceptionHa
         }
 
         final String errorMessage = MessageFormat.format("DemoSRENoContentException: {0}", response);
-        log.info(errorMessage);
+        log.error(errorMessage);
         return handleExceptionInternal(exception, response, new HttpHeaders(),
                 HttpStatus.NO_CONTENT, request);
+    }
+
+    @ExceptionHandler(DemoSREMaxRetriesExceededException.class)
+    public ResponseEntity<Object> handleDemoSREMaxRetriesExceededException(final DemoSREMaxRetriesExceededException exception,
+                                                                  final WebRequest request) {
+
+        String exceptionMessage = exception.getMessage();
+
+        if(!StringUtils.hasText(exceptionMessage)) {
+            exceptionMessage = messageSourceService.getMessage("demo-sre.service-unavailable");
+        }
+
+        HttpStatus httpStatus = HttpStatus.SERVICE_UNAVAILABLE;
+        MetaDataEnvelope response =
+                new MetaDataEnvelope(httpStatus.toString(), ErrorCode.DEMOSRE_MAX_RETRIES_EXCEEDED, exceptionMessage);
+
+        if (!isEmpty(exception.getErrors())) {
+            final MetaData meta = new MetaData(httpStatus.toString());
+            response = new MetaDataEnvelope(meta, exception.getErrors());
+        }
+
+        final String errorMessage = MessageFormat.format("handleDemoSREMaxRetriesExceededException: {0}", response);
+        log.error(errorMessage);
+        return handleExceptionInternal(exception, response, new HttpHeaders(),
+                httpStatus, request);
     }
 
 }
--- src/main/java/br/com/bradescoseguros/opin/interfaceadapter/gateway/DemoSREGatewayImpl.java
+++ src/main/java/br/com/bradescoseguros/opin/interfaceadapter/gateway/DemoSREGatewayImpl.java
@@ -1,5 +1,6 @@
 package br.com.bradescoseguros.opin.interfaceadapter.gateway;
 
+import br.com.bradescoseguros.opin.businessrule.exception.demosre.DemoSREMaxRetriesExceededException;
 import br.com.bradescoseguros.opin.businessrule.gateway.DemoSREGateway;
 import br.com.bradescoseguros.opin.domain.demosre.DemoSRE;
 import br.com.bradescoseguros.opin.external.configuration.redis.RedisConstants;
@@ -8,9 +9,11 @@ import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
@@ -21,7 +24,7 @@ public class DemoSREGatewayImpl implements DemoSREGateway {
     private DemoSRERepository repository;
 
     @Override
-    @Retry(name = "CosmoRetry")
+    @Retry(name = "CosmoRetry", fallbackMethod = "fallbackCosmoRetry")
     @Cacheable(cacheNames = RedisConstants.DERMOSRE_CACHE_NAME)
     public Optional<DemoSRE> findById(final Integer id) {
         return repository.findById(id);
@@ -50,5 +53,9 @@ public class DemoSREGatewayImpl implements DemoSREGateway {
         restTemplate.exchange("", HttpMethod.POST, null, String.class);
 
         return null;
+    }
+
+    private Optional<DemoSRE> fallbackCosmoRetry(final Integer id, DataAccessResourceFailureException e) {
+        throw new DemoSREMaxRetriesExceededException();
     }
 }
