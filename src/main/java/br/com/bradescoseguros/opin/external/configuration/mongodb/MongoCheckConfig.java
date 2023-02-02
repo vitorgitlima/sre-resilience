3a5ba716f7b72a3d92c4fdbe8c77a02104f37098
Edson Roberto Faccio Junior <edsonf@ciandt.com>
2023-02-02 11:17:54 -0300

    ajustes e criação inicial do circuit breaker

--- src/main/java/br/com/bradescoseguros/opin/external/configuration/mongodb/MongoCheckConfig.java
+++ src/main/java/br/com/bradescoseguros/opin/external/configuration/mongodb/MongoCheckConfig.java
@@ -1,20 +1,20 @@
-package br.com.bradescoseguros.opin.external.configuration.mongodb;
-
-import br.com.bradescoseguros.opin.interfaceadapter.repository.DemoSRERepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class MongoCheckConfig {
-
-    private final DemoSRERepository demoSRERepository;
-
-    @Scheduled(fixedDelay = 60000)
-    public void scheduleFixedDelayTask() {
-        log.info("Check connection mongo - " + demoSRERepository.count());
-    }
-}
+//package br.com.bradescoseguros.opin.external.configuration.mongodb;
+//
+//import br.com.bradescoseguros.opin.interfaceadapter.repository.DemoSRERepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class MongoCheckConfig {
+//
+//    private final DemoSRERepository demoSRERepository;
+//
+//    @Scheduled(fixedDelay = 60000)
+//    public void scheduleFixedDelayTask() {
+//        log.info("Check connection mongo - " + demoSRERepository.count());
+//    }
+//}
