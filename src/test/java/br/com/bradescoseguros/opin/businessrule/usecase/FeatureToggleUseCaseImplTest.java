f6a260cc8a23370a3736a966dff761e5e40746b7
VitorLima <vitorgl@ciandt.com>
2023-05-30 15:56:46 -0300

    implementa feature_toggle test unitario, caso de sucesso

--- src/test/java/br/com/bradescoseguros/opin/businessrule/usecase/FeatureToggleUseCaseImplTest.java
+++ src/test/java/br/com/bradescoseguros/opin/businessrule/usecase/FeatureToggleUseCaseImplTest.java
@@ -1,11 +1,11 @@
 package br.com.bradescoseguros.opin.businessrule.usecase;
 
-import br.com.bradescoseguros.opin.businessrule.gateway.CrudGateway;
+import br.com.bradescoseguros.opin.businessrule.gateway.FeatureToggleGateway;
 import br.com.bradescoseguros.opin.domain.DemoSRE;
 import br.com.bradescoseguros.opin.domain.ExecutionResult;
 import br.com.bradescoseguros.opin.dummy.DummyObjectsUtil;
 import com.azure.spring.cloud.feature.management.FeatureManager;
-import org.junit.jupiter.api.BeforeEach;
+import com.mongodb.assertions.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,6 +16,7 @@ import reactor.core.publisher.Mono;
 import java.util.Optional;
 
 import static com.mongodb.assertions.Assertions.assertNull;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
@@ -23,30 +24,30 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class FeatureToggleUseCaseImplTest {
 
+    @InjectMocks
+    private FeatureToggleUseCaseImpl featureToggleUseCase;
     @Mock
-    private CrudGateway mockGateway;
+    private FeatureToggleGateway mockGateway;
     @Mock
     private FeatureManager featureManager;
 
-    @InjectMocks
-    private FeatureToggleUseCaseImpl featureToggleUseCase;
 
     @Test
     void getDemoSREWithToggleEnabled_ReturnValidValue() {
         // Arrange
         final int id = 2;
-        Optional<DemoSRE> demoSREMock = Optional.of(DummyObjectsUtil.newInstance(DemoSRE.class));
+        Optional<DemoSRE> demoSREMock = Optional.of(new DemoSRE(2,"a"));
         when(featureManager.isEnabledAsync("feature-a")).thenReturn(Mono.just(true));
-//        when(mockGateway.findById(1)).thenReturn(Optional.of(new DemoSRE()));
-        when(mockGateway.findById(anyInt())).thenReturn(demoSREMock);
+        when(mockGateway.findByIdWithFeatureToggle(anyInt())).thenReturn(demoSREMock);
 
         // Act
         ExecutionResult<DemoSRE> result = featureToggleUseCase.getDemoSREWithToggleEnabled(id);
 
         // Assert
+        assertThat(result.getObject().getId()).isEqualTo(2);
         assertNotNull(result.getObject());
         assertNull(result.getErrorType());
         verify(featureManager).isEnabledAsync("feature-a");
-        verify(mockGateway, times(1)).findById(anyInt());
+        verify(mockGateway, times(1)).findByIdWithFeatureToggle(anyInt());
     }
 }
